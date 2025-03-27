b7323e1333b138b7446b5b5e3eed37fbc124f94c
jmalinie <38893005+jmalinie@users.noreply.github.com>
2025-03-27 18:11:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
+import os
 
 app = Flask(__name__)
 
@@ -15,6 +16,8 @@ def get_subtitles():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
